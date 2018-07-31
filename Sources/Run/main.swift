ad654722987bb30a1e1ea46c2a8cc47f779d7b50
Liu <liumin2011@gmail.com>
2018-07-31 16:54:56 +0800

    在非 Debug 环境下，无需指定IP和端口地址

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,9 +1,14 @@
 import App
 
 
-
-
+#if DEBUG
 var arguments = CommandLine.arguments
 arguments.append(contentsOf: ["--hostname","127.0.0.1","--port","8181"])
 
-try app(.detect(arguments: arguments)).run()
+#else
+
+try app(.detect()).run()
+
+#endif
+
+
