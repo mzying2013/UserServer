1ea9c7a9c171f40900296d2de0428e5eeed3a66d
Liu <liumin2011@gmail.com>
2018-08-01 16:21:35 +0800

    删除配置的 hostname 和 port

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,14 +1,13 @@
 import App
 
 
-var arguments = CommandLine.arguments
-arguments.append(contentsOf: ["--hostname","127.0.0.1","--port","8069"])
+//var arguments = CommandLine.arguments
+//arguments.append(contentsOf: ["--hostname","127.0.0.1","--port","8069"])
+//try app(.detect(arguments: arguments)).run()
+//
+//
 
-try app(.detect(arguments: arguments)).run()
 
-
-
-
-//try app(.detect()).run()
+try app(.detect()).run()
 
 
